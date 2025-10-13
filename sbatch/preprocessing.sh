1c4b049369b1dac748b6418bc6b8606c9c141d87
Jan Lewandowski <283003@student.pwr.edu.pl>
2025-10-13 10:21:18 -0400

    feat: roght now preprocess on CPUs

--- sbatch/preprocessing.sh
+++ sbatch/preprocessing.sh
@@ -3,20 +3,19 @@
 #SBATCH -n 1               # Number of tasks. ALWAYS set to 1
 #SBATCH -c 32              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
 #SBATCH -t 1:0:0           # Number of hours to run (H:M:S). Change as needed.
-#SBATCH -A cis220051-gpu   # The TDM account to charge for this. Don't change.
-#SBATCH -p gpu             # Partition to use.
-#SBATCH --gpus-per-node=1  # Must use just one GPU.  Do not change!
+#SBATCH -A cis220051   # The TDM account to charge for this. Don't change.
+#SBATCH -p shared             # Partition to use.
 
 # These three lines "load" the TDM python.  Almost always keep them.
 module use /anvil/projects/tdm/opt/core
 module load tdm
 module load python/seminar r/seminar
 
-SCRIPT_DIR="$(dirname -- "$(readlink -f "${BASH_SOURCE}")")"
-cd $SCRIPT_DIR/..
+cd $SLURM_SUBMIT_DIR
 
-python3 preprocessing/crop_faces.py >logs/crop_faces.log 2>logs/crop_faces.err
+python3 preprocessing/crop_faces.py >logs/crop_faces_${SLURM_JOBID}.log 2>&1
 
+# Make sure you run sbatch from the root of the project!
 # sbatch commands:
 # sbatch preprocessing.sh
 # squeue --me
