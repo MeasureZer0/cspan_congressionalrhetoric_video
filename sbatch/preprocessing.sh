3e639bbc7882e9bdc68955aa5bb5badbe5e52ea5
Jan Lewandowski <lewandowski.jan@o2.pl>
2025-10-13 17:26:35 +0200

    fix: small fixes

--- sbatch/preprocessing.sh
+++ sbatch/preprocessing.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -N 1               # Number of nodes. ALWAYS set to 1
 #SBATCH -n 1               # Number of tasks. ALWAYS set to 1
-#SBATCH -c 32              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
+#SBATCH -c 16              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
 #SBATCH -t 1:0:0           # Number of hours to run (H:M:S). Change as needed.
-#SBATCH -A cis220051   # The TDM account to charge for this. Don't change.
-#SBATCH -p shared             # Partition to use.
+#SBATCH -A cis220051       # The TDM account to charge for this. Don't change.
+#SBATCH -p shared          # Partition to use.
 
 # These three lines "load" the TDM python.  Almost always keep them.
 module use /anvil/projects/tdm/opt/core
