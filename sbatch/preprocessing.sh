6f7ebce2c7e99b08db5c13baa9a19aa8d5d10bf9
Jan Lewandowski <283003@student.pwr.edu.pl>
2025-10-24 08:58:58 -0400

    feat: sbatch script refine

--- sbatch/preprocessing.sh
+++ sbatch/preprocessing.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH -N 1               # Number of nodes. ALWAYS set to 1
 #SBATCH -n 1               # Number of tasks. ALWAYS set to 1
-#SBATCH -c 16              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
+#SBATCH -c 32              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
 #SBATCH -t 1:0:0           # Number of hours to run (H:M:S). Change as needed.
 #SBATCH -A cis220051       # The TDM account to charge for this. Don't change.
 #SBATCH -p shared          # Partition to use.
@@ -14,7 +14,7 @@ module load python/seminar r/seminar
 cd $SLURM_SUBMIT_DIR
 
 # -u to disable stdout buffering
-python3 -u preprocessing/crop_faces.py >logs/crop_faces_${SLURM_JOBID}.log 2>&1
+python3 -u preprocessing/crop_faces.py --purge >logs/crop_faces_${SLURM_JOBID}.log 2> logs/crop_faces_${SLURM_JOBID}.err
 
 # Make sure you run sbatch from the root of the project!
 # sbatch commands:
