becd4bbd24bfbaa8a00890fd76e0cef1c7ec0b03
Jan Lewandowski <283003@student.pwr.edu.pl>
2025-10-24 09:02:58 -0400

    fix: set appropriate time allocation in sbatch

--- sbatch/preprocessing.sh
+++ sbatch/preprocessing.sh
@@ -2,7 +2,7 @@
 #SBATCH -N 1               # Number of nodes. ALWAYS set to 1
 #SBATCH -n 1               # Number of tasks. ALWAYS set to 1
 #SBATCH -c 32              # Number of CPU cores. Can go as high as 128. Each additional CPU core adds around 1.9GB of RAM so to get more memory, add more CPU cores.
-#SBATCH -t 1:0:0           # Number of hours to run (H:M:S). Change as needed.
+#SBATCH -t 0:15:0           # Number of hours to run (H:M:S). Change as needed.
 #SBATCH -A cis220051       # The TDM account to charge for this. Don't change.
 #SBATCH -p shared          # Partition to use.
 
